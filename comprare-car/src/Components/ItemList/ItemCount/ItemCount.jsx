e1d3968d097103cfaa7036709d0d5f21bb46932e
MatiasL <longomaty@gmail.com>
2021-07-12 18:41:00 -0300

    last commit

--- comprare-car/src/Components/ItemList/ItemCount/ItemCount.jsx
+++ comprare-car/src/Components/ItemList/ItemCount/ItemCount.jsx
@@ -3,27 +3,31 @@ import React, { useState } from 'react';
 
 
 
-function ItemCount({stock, initital, addOn, remove}){
+const ItemCount = (stock, initital) => {
     const [count, setCount] = useState(0);
-function addOn(){
-    if(initital >= 1 && count < stock){
-        setCount(count + 1);
+
+
+    function addOn(){
+        if(initital >= 1 && count < stock){
+            setCount(count + 1);
+        }
     }
-        
-    
+
+    function remove(){
+        if(count > 1){
+            setCount(count - 1);
+            }
+        }
+
+
+        return (<div className="counter">
+                    <h3>Cantidad: {count}</h3>
+                    <button onClick = {remove} className="minus">-</button>
+                    <button onClick = {addOn} className="plus">+</button>
+                </div>)
 }
 
-function remove(){
-    if(count > 1){
-        setCount(count - 1);
-    }
-}
 
-    return(<div className="counter">
-        <h3>Cantidad: {count}</h3>
-        <button onClick = {remove} className="minus">-</button>
-        <button onClick = {addOn} className="plus">+</button>
-    </div>)
-}
+
 
 export default ItemCount;
